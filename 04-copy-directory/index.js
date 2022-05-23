0dbf3f901f95a18191cd5fedf27ee5703fe93257
MrUnderSan <MrUnderSan@gmail.com>
2022-05-23 22:59:09 +0300

    feat: end 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const fs = require("fs");
 
+const fsPromises = fs.promises;
+
+fsPromises
+  .mkdir("04-copy-directory/files-copy", { recursive: true })
+  .then(function () {})
+  .catch(function () {
+    console.log("failed to create directory");
+  });
+
+fs.readdir("04-copy-directory/files/", (err, files) => {
+  if (err) throw err;
+
+  files.forEach((elem) => {
+    fsPromises
+      .copyFile(
+        `04-copy-directory/files/${elem}`,
+        `04-copy-directory/files-copy/${elem}`
+      )
+      .then(function () {})
+      .catch(function (error) {
+        console.log(error);
+      });
+  });
+});
