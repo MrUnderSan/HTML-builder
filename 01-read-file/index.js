0d7f183c3761945b5113755a86295602a98a7c04
MrUnderSan <MrUnderSan@gmail.com>
2022-05-25 20:24:18 +0300

    feat: add ReadStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,10 @@
-let fs = require("fs");
-
-fs.readFile("01-read-file/text.txt", "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+const fs = require("fs");
+const path = require("path");
+const readableStream = fs.createReadStream(
+  path.join(__dirname, "text.txt"),
+  "utf-8"
+);
+let data = "";
+readableStream.on("data", (chunk) => (data += chunk));
+readableStream.on("end", () => console.log(data));
+readableStream.on("error", (error) => console.log("Error", error.message));
